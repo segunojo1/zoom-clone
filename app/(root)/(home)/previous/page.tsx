42a23a34ab6888a8415dab0b5c9104e743df9e94
Segun Ojo <olusegunkd2017@gmail.com>
2026-01-13 16:55:00 +0100

    fix: colors

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -3,7 +3,7 @@ import CallList from '@/components/CallList'
 const Previous = () => {
   return (
     <div>
-      <h1>Previous</h1>
+      <h1 className='text-3xl font-bold'>Previous</h1>
 
       <CallList type='ended' />
     </div>
--- components/meeting-card.tsx
+++ components/meeting-card.tsx
@@ -26,13 +26,13 @@ const MeetingCard = ({
   buttonText,
 }: MeetingCardProps) => {
   return (
-    <section className='flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-dark-1 px-5 py-8 xl:max-w-[568px]'>
+    <section className='flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-[#161925] px-5 py-8 xl:max-w-[568px]'>
       <article className='flex flex-col gap-5'>
         <Image
           src={icon}
           alt='upcoming'
-          width={400}
-          height={300}
+          width={28}
+          height={28}
         />
         <div className='flex justify-between'>
           <div className='flex flex-col gap-2'>
@@ -54,14 +54,14 @@ const MeetingCard = ({
               style={{ top: 0, left: index * 28 }}
             />
           ))}
-          <div className='flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4'>
+          <div className='flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-[#252A41] bg-[#1E2757]'>
             +5
           </div>
         </div>
         {
           !isPreviousMeeting && (
             <div className="flex gap-2">
-            <Button onClick={handleClick} className="rounded bg-blue-1 px-6">
+            <Button onClick={handleClick} className="rounded bg-[#0E78F9] px-6">
               {buttonIcon1 && (
                 <Image src={buttonIcon1} alt="feature" width={20} height={20} />
               )}
@@ -72,7 +72,7 @@ const MeetingCard = ({
                 navigator.clipboard.writeText(link);
                 toast("Link Copied");
               }}
-              className="bg-dark-4 px-6"
+              className="bg-[#1E2757] px-6"
             >
               <Image
                 src="/icons/copy.svg"
